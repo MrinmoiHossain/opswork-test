54e986e55c088f33fea91f5164cae90ced54c655
Mokit Hossain <mokit.aust@gmail.com>
2020-12-18 23:53:33 +0600

    yum repo add

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -1,5 +1,15 @@
+#Install Nginx
+yum_repository 'nginx' do
+  description  'Nginx.org Repository'
+  baseurl      "https://nginx.org/packages/centos/$releasever/$basearch/"
+  action       :create
+end
+
 execute "yum update -y"
 
 package ["vim", "wget", "curl", "epel-release"]
 
-package [ "nginx" ]
+package 'nginx' do
+  version '1.18.0-1.el7.ngx'
+  action :install
+end
