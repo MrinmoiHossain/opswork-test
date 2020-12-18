81f2babaeb27bb5fbb179f35b94f5374fd5520af
Mokit Hossain <mokit.aust@gmail.com>
2020-12-18 22:16:21 +0600

    change

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -1,12 +1,5 @@
-selinux_state "SELinux Permissive" do
-    action :permissive
-end
-
-execute 'yum update -y'
+execute "yum update -y"
 
 package ["vim", "wget", "curl", "epel-release"]
 
-package "nginx" do
-    version "1.18.0-1.el7.ngx"
-    action :install
-end 
+package [ "nginx" ]
